f156178d12ef9c947577e74754813fadef6a48a9
rathakarn <gkrgroup.co.th@gmail.com>
2019-04-09 12:58:30 +0700

    Initial Commit

---
+++ My Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Tutorial
+//
+//  Created by korakod Chavanaviraj on 9/4/2562 BE.
+//  Copyright © 2562 GKRGROUP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
