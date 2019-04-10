2925c5a7818049f6f901500964160f14c4cdc6bb
rathakarn <gkrgroup.co.th@gmail.com>
2019-04-10 10:00:05 +0700

    Create ViewController Success

---
+++ My Tutorial/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  My Tutorial
+//
+//  Created by korakod Chavanaviraj on 10/4/2562 BE.
+//  Copyright © 2562 GKRGROUP. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
